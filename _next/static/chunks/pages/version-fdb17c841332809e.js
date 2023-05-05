e2961a396764df422b4a3e93a1f1872c055bf2b5
Md Arif Hossain <arif.swfu@outlook.com>
2023-05-05 23:39:05 +0600

    Add files via upload

---
+++ _next/static/chunks/pages/version-fdb17c841332809e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[338],{8277:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/version",function(){return _(4086)}])},4086:function(n,u,_){"use strict";_.r(u);var e=_(5893);_(7294);let i=()=>(0,e.jsx)("div",{children:"1.0.0"});u.default=i}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8277)}),_N_E=n.O()}]);
